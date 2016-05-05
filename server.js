ec8df01510a250e063ee1ce1be008e3b9a8f6d79
lailai00 <lailai00@hotmail.com>
2016-05-05 11:32:45 +0800

    初始化

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var swig  = require('swig');
+var React = require('react');
+var Router = require('react-router');
+var routes = require('./app/routes');
+var mongoose = require('mongoose');
+var Character = require('./models/character');
+var config = require('./config');
+var async = require('async');
+var request = require('request');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res) {
+  Router.run(routes, req.path, function(Handler) {
+    var html = React.renderToString(React.createElement(Handler));
+    var page = swig.renderFile('views/index.html', { html: html });
+    res.send(page);
+  });
+});
+
+
+mongoose.connect(config.database);
+mongoose.connection.on('error', function() {
+  console.info('Error: Could not connect to MongoDB. Did you forget to run `mongod`?');
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
